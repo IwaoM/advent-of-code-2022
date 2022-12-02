5a121a051ec525a0324ba35068e58b69a6862165
“IwaoM” <iwao27@gmail.com>
2022-12-02 13:17:44 +0100

    day2 part 2

--- day2.js
+++ day2.js
@@ -7,18 +7,35 @@ module.exports = function day2 (inputData) {
   let result1 = 0;
 
   for (let i = 0; i < inputArray.length; i++) {
+    if (!inputArray[i]) {
+      break;
+    }
+
     // points for the shape : rock == 1, paper == 2, scissors == 3
     const shape1 = letterList1.indexOf(inputArray[i].split(" ")[0]) + 1;// rock == 1, paper == 2, scissors == 3
     const shape2 = letterList2.indexOf(inputArray[i].split(" ")[1]) + 1;
     let roundPoints = shape2;
 
     // points for the outcome
-    if (shape1 && shape2 && shape1 == shape2) {
+    if (shape1 == shape2) {
       roundPoints += 3;
-    } else if (shape2 - shape1 % 3 == 1) {
+    } else if (shape2 - shape1 % 3 == 1) { // 2 beats 1, 3 beats 2, 1 beats 3
       roundPoints += 6;
     }
     result1 += roundPoints;
   }
-  console.log(`Total number of points: ${result1}`);
+  console.log(`Total number of points for part 1: ${result1}`);
+
+  // Part 2
+  let result2 = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (!inputArray[i]) {
+      break;
+    }
+
+    const outcomePoints = letterList2.indexOf(inputArray[i].split(" ")[1]) * 3;
+    const shapePoints = (letterList1.indexOf(inputArray[i].split(" ")[0]) + letterList2.indexOf(inputArray[i].split(" ")[1]) + 2) % 3 + 1;
+    result2 += outcomePoints + shapePoints;
+  }
+  console.log(`Total number of points for part 2: ${result2}`);
 };
