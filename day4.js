eec5fc37c73f989ea1da4d92c275554087588db9
“IwaoM” <iwao27@gmail.com>
2022-12-04 18:27:21 +0100

    day 4 part 2

--- day4.js
+++ day4.js
@@ -3,6 +3,7 @@ module.exports = function day4 (inputData) {
 
   // Part 1
   let result1 = 0;
+  let result2 = 0;
   // console.log(`result1 = ${result1}\n`);
   for (let i = 0; i < inputArray.length; i++) {
     // parsing
@@ -10,14 +11,21 @@ module.exports = function day4 (inputData) {
     const elf1Start = parseInt(elf1.split("-")[0]), elf1End = parseInt(elf1.split("-")[1]);
     const elf2Start = parseInt(elf2.split("-")[0]), elf2End = parseInt(elf2.split("-")[1]);
 
-    // checking if one elf's range of sections fully contains the other
+    //* Part 1
+    // check if one elf's range of sections fully contains the other
     if (elf1Start <= elf2Start && elf1End >= elf2End || elf2Start <= elf1Start && elf2End >= elf1End) {
       result1++;
     }
+
+    //* Part 2
+    // check if the ranges overlap
+    if (
+      elf1Start <= elf2Start && elf2Start <= elf1End || // case 1: e1S <= e2S <= e1E <= e2E
+      elf2Start <= elf1Start && elf1Start <= elf2End // case 2: e2S <= e1S <= e2E <= e1E
+    ) {
+      result2++;
+    }
   }
   console.log(`Number of assignment pairs in which one range fully contains the other: ${result1}`);
-
-  // Part 2
-  let result2 = 0;
-  console.log(`Sum of the priorities of badges: ${result2}`);
+  console.log(`Number of assignment pairs that overlap: ${result2}`);
 };
