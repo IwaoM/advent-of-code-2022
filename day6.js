d175e5f030562459ceab35876d896b5ee881ba9a
“IwaoM” <iwao27@gmail.com>
2022-12-06 10:58:58 +0100

    day 6 part 2

--- day6.js
+++ day6.js
@@ -1,18 +1,21 @@
 module.exports = function day6 (inputData) {
   // No parsing needed :o
 
-  //* Part 1
-  let result1;
-  for (let i = 4; i < inputData.length; i++) {
-    const fourCharsToSet = new Set(inputData.slice(i - 4, i));
-    if (fourCharsToSet.size === 4) {
-      result1 = i;
-      break;
+  // turns out the algo for the 1st part can be used for the 2nd as well, so we turn it into a function
+  function getMarkerIndex (markerLength, signal) {
+    for (let i = markerLength; i < signal.length; i++) {
+      const xCharsToSet = new Set(inputData.slice(i - markerLength, i));
+      if (xCharsToSet.size === markerLength) {
+        return i;
+      }
     }
   }
+
+  //* Part 1
+  let result1 = getMarkerIndex(4, inputData);
   console.log(`The first start-of-packet marker is after character ${result1}`);
 
   //* Part 2
-  let result2;
+  let result2 = getMarkerIndex(14, inputData);
   console.log(`result2: ${result2}`);
 };
