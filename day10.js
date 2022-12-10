baa36744d3ee9ba7453a4967e3ad15619ebf1cc2
“IwaoM” <iwao27@gmail.com>
2022-12-10 17:55:20 +0100

    feat: add day 9 part 1

---
+++ day10.js
@@ -0,0 +1,45 @@
+class Cpu {
+  constructor () {
+    this.register = [1];
+    this.clock = 0;
+  }
+
+  tick (newVal) {
+    this.clock++; // cycle begins : cycle number is incremented
+    this.register.push(newVal ? newVal : this.register[this.register.length - 1]); // cycle ends : register is updated
+  }
+
+  noop () {
+    this.tick();
+  }
+
+  addX (valToAdd) {
+    this.tick();
+    const newVal = this.register[this.register.length - 1] + valToAdd;
+    this.tick(newVal);
+  }
+
+  registerDuringCycle (cycleNum) {
+    return this.register[cycleNum - 1]; // the value during a given cycle is the last stored value before a new one is pushed at the end of the cycle
+  }
+}
+
+module.exports = function day10 (inputData) {
+  const inputArray = inputData.split("\n");
+  inputArray.pop(); // remove empty line at EOF
+
+  //* Part 1
+  const part1Cpu = new Cpu();
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === "noop") {
+      part1Cpu.noop();
+    } else {
+      part1Cpu.addX(parseInt(inputArray[i].split(" ")[1]));
+    }
+  }
+  let result1 = 0;
+  for (let i = 20; i <= 220; i += 40) {
+    result1 += part1Cpu.registerDuringCycle(i) * i;
+  }
+  console.log(`Sum of the signal strengths during cycles 20, 60, 100, 140, 180 and 220: ${result1}`);
+};
