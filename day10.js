bd278b15989b110897c8cb23d8f8948c5f90b97d
“IwaoM” <iwao27@gmail.com>
2022-12-10 18:49:47 +0100

    feat: add day 10 part 2

--- day10.js
+++ day10.js
@@ -2,11 +2,25 @@ class Cpu {
   constructor () {
     this.register = [1];
     this.clock = 0;
+    this.crtScreen = []; // part 2
   }
 
   tick (newVal) {
     this.clock++; // cycle begins : cycle number is incremented
-    this.register.push(newVal ? newVal : this.register[this.register.length - 1]); // cycle ends : register is updated
+
+    // part 2 : draw a pixel in the screen
+    const posInRow = (this.clock - 1) % 40;
+    const currentRegister = this.register[this.register.length - 1];
+    if (!posInRow) { // start of a new row
+      this.crtScreen.push("");
+    }
+    if (currentRegister - 1 <= posInRow && posInRow <= currentRegister + 1) {
+      this.crtScreen[this.crtScreen.length - 1] += "#";
+    } else {
+      this.crtScreen[this.crtScreen.length - 1] += ".";
+    }
+
+    this.register.push(newVal ? newVal : currentRegister); // cycle ends : register is updated
   }
 
   noop () {
@@ -22,6 +36,12 @@ class Cpu {
   registerDuringCycle (cycleNum) {
     return this.register[cycleNum - 1]; // the value during a given cycle is the last stored value before a new one is pushed at the end of the cycle
   }
+
+  drawScreen () {
+    for (let i = 0; i < 6; i++) {
+      console.log(this.crtScreen[i]);
+    }
+  }
 }
 
 module.exports = function day10 (inputData) {
@@ -29,17 +49,21 @@ module.exports = function day10 (inputData) {
   inputArray.pop(); // remove empty line at EOF
 
   //* Part 1
-  const part1Cpu = new Cpu();
+  const cpu = new Cpu();
   for (let i = 0; i < inputArray.length; i++) {
     if (inputArray[i] === "noop") {
-      part1Cpu.noop();
+      cpu.noop();
     } else {
-      part1Cpu.addX(parseInt(inputArray[i].split(" ")[1]));
+      cpu.addX(parseInt(inputArray[i].split(" ")[1]));
     }
   }
   let result1 = 0;
   for (let i = 20; i <= 220; i += 40) {
-    result1 += part1Cpu.registerDuringCycle(i) * i;
+    result1 += cpu.registerDuringCycle(i) * i;
   }
   console.log(`Sum of the signal strengths during cycles 20, 60, 100, 140, 180 and 220: ${result1}`);
+
+  //* Part 2
+  console.log("");
+  cpu.drawScreen();
 };
