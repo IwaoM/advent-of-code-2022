4692374452cdb54d531f58bdcfbe8de35f826bdb
“IwaoM” <iwao27@gmail.com>
2022-12-17 19:00:50 +0100

    feat: add day 15 part 2

--- day15.js
+++ day15.js
@@ -2,8 +2,6 @@ class Sensor {
   static sensorList = [];
   static xMax;
   static xMin;
-  static yMax;
-  static yMin;
 
   static clearSensors () {
     this.sensorList = [];
@@ -23,12 +21,6 @@ class Sensor {
     if (Sensor.xMin == null || Sensor.xMin > this.x - this.range) {
       Sensor.xMin = this.x - this.range;
     }
-    if (Sensor.yMax == null || Sensor.yMax < this.y + this.range) {
-      Sensor.yMax = this.y + this.range;
-    }
-    if (Sensor.yMin == null || Sensor.yMin > this.y - this.range) {
-      Sensor.yMin = this.y - this.range;
-    }
   }
 
   coversCoords (x, y) {
@@ -44,6 +36,17 @@ class Sensor {
       }
     }
     return covered;
+  }
+
+  static getSensorCoveringCoords (x, y) { // used for part 2 since bruteforcing part 1's function would take too much time
+    let coveringSensor = null;
+    for (let i = 0; i < Sensor.sensorList.length; i++) {
+      if (Sensor.sensorList[i].coversCoords(x, y)) {
+        coveringSensor = Sensor.sensorList[i];
+        break;
+      }
+    }
+    return coveringSensor;
   }
 
   static beaconExistsInCoords (x, y) {
@@ -77,7 +80,6 @@ module.exports = function day15 (inputData) {
   let date2 = new Date();
 
   console.log(`xMin = ${Sensor.xMin}, xMax = ${Sensor.xMax}`);
-  console.log(`yMin = ${Sensor.yMin}, yMax = ${Sensor.yMax}`);
   console.log(`Execution time: ${(date2 - date1) / 1000}s\n`);
 
   //* Part 1
@@ -95,4 +97,40 @@ module.exports = function day15 (inputData) {
 
   console.log(`Number of positions that cannot contain a beacon in row y = ${checkedY}: ${result1}`);
   console.log(`Execution time: ${(date2 - date1) / 1000}s\n`);
+
+  //* Part 2
+  console.log(`== PART 2 ==`);
+  date1 = new Date();
+
+  const coordMin = 0, coordMax = 4000000;
+  let result2 = 0, beaconCoords = [];
+
+  // bruteforcing part 1's approach would take too much time (something like 12 or 13 hours ?)
+  // instead, use a new, more optimized strategy
+
+  let j = coordMin;
+  while (j <= coordMax) {
+    let i = coordMin;
+    while (i <= coordMax) {
+      const coveringSensor = Sensor.getSensorCoveringCoords(i, j);
+      if (coveringSensor) {
+        i = coveringSensor.x + coveringSensor.range - Math.abs(coveringSensor.y - j);
+        // the above operation makes i jump to the last position in the current row covered by coveringSensor
+      } else {
+        beaconCoords.push(i, j);
+        break;
+      }
+      i++;
+    }
+    if (beaconCoords.length) {
+      break;
+    }
+    j++;
+  }
+
+  result2 = 4000000 * beaconCoords[0] + beaconCoords[1];
+  date2 = new Date();
+
+  console.log(`Tuning frequency for the distress beacon in [${beaconCoords[0]}, ${beaconCoords[1]}] : ${result2}`);
+  console.log(`Execution time: ${(date2 - date1) / 1000}s`);
 };
